5a9f7690ef82e7df0342021b27ba10ca6e52e0f2
Jiří Čtvrtka <jiri.ctvrtka.sde@gmail.com>
2020-08-12 11:15:57 +0200

    PMM-5684 Remove test.

--- mysqld_exporter_test.go
+++ mysqld_exporter_test.go
@@ -15,7 +15,6 @@ import (
 	"reflect"
 	"regexp"
 	"runtime"
-	"strconv"
 	"strings"
 	"syscall"
 	"testing"
@@ -23,7 +22,6 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/smartystreets/goconvey/convey"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestParseMycnf(t *testing.T) {
@@ -418,47 +416,4 @@ func getBody(urlToGet string) ([]byte, error) {
 	}
 
 	return body, nil
-}
-
-func Test_disableCache(t *testing.T) {
-	name := "test_cache"
-	db, err := connectDB("root@tcp(127.0.0.1:3306)/")
-	assert.NoError(t, err)
-	defer db.Close()
-
-	var dbExist int
-	err = db.QueryRow("SELECT COUNT(SCHEMA_NAME) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '" + name + "'").Scan(&dbExist)
-	assert.NoError(t, err)
-	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + name)
-	assert.NoError(t, err)
-	if dbExist == 0 {
-		defer func() {
-			_, err = db.Exec("DROP DATABASE " + name)
-			assert.NoError(t, err)
-		}()
-	}
-
-	var tableExist int
-	err = db.QueryRow("SELECT COUNT(TABLE_NAME) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" + name + "' AND TABLE_NAME = '" + name + "'").Scan(&tableExist)
-	assert.NoError(t, err)
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS " + name + "." + name + " (id int(64))")
-	assert.NoError(t, err)
-	if tableExist == 0 {
-		defer func() {
-			_, err = db.Exec("DROP TABLE " + name + "." + name)
-			assert.NoError(t, err)
-		}()
-	}
-	_, err = db.Exec("TRUNCATE " + name + "." + name)
-	assert.NoError(t, err)
-
-	var count int
-	rows := []int{1, 2}
-	for _, row := range rows {
-		_, err = db.Exec("INSERT INTO " + name + "." + name + " VALUES(" + strconv.Itoa(row) + ")")
-		assert.NoError(t, err)
-		err = db.QueryRow("SELECT TABLE_ROWS FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" + name + "' AND TABLE_NAME = '" + name + "'").Scan(&count)
-		assert.NoError(t, err)
-		assert.Equal(t, row, count)
-	}
 }
