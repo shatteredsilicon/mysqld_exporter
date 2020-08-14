b0ee0cbba6c30277621e158cc66890b67a6fb478
Jiří Čtvrtka <jiri.ctvrtka.sde@gmail.com>
2020-08-14 11:58:27 +0200

    PMM-5684 Required changes.

--- collector/info_schema_tables_test.go
+++ collector/info_schema_tables_test.go
@@ -5,6 +5,7 @@ package collector
 import (
 	"context"
 	"database/sql"
+	"reflect"
 	"testing"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -13,7 +14,9 @@ import (
 
 func TestScrapeTableSchema(t *testing.T) { //nolint:unused
 	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer db.Close()
 
 	dbName := "test_cache_db"
@@ -21,20 +24,28 @@ func TestScrapeTableSchema(t *testing.T) { //nolint:unused
 	*tableSchemaDatabases = dbName
 
 	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + dbName)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer func() { //nolint:wsl
 		_, err = db.Exec("DROP DATABASE " + dbName)
-		assert.NoError(t, err)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}()
 
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS " + dbName + "." + tableName + " (id int(64))")
 	assert.NoError(t, err)
 	defer func() { //nolint:wsl
 		_, err = db.Exec("DROP TABLE " + dbName + "." + tableName)
-		assert.NoError(t, err)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}()
 	_, err = db.Exec("TRUNCATE " + dbName + "." + tableName)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	ctx := context.Background()
 	addRowAndCheckRowsCount(t, ctx, db, dbName, tableName, 1)
@@ -43,7 +54,9 @@ func TestScrapeTableSchema(t *testing.T) { //nolint:unused
 
 func addRowAndCheckRowsCount(t *testing.T, ctx context.Context, db *sql.DB, dbName, tableName string, expectedRowsCount float64) { //nolint:go-lint
 	_, err := db.Exec("INSERT INTO " + dbName + "." + tableName + " VALUES(50)")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	ch := make(chan prometheus.Metric)
 	go func() { //nolint:wsl
 		if err = (ScrapeTableSchema{}).Scrape(ctx, db, ch); err != nil {
@@ -52,13 +65,15 @@ func addRowAndCheckRowsCount(t *testing.T, ctx context.Context, db *sql.DB, dbNa
 		close(ch)
 	}()
 
+	// For test is important only second recieve to channel.
+	// Others can be ignored.
 	<-ch
 	got := readMetric(<-ch)
 	<-ch
 	<-ch
 	<-ch
 
-	expected := MetricResult{
+	expect := MetricResult{
 		labels: labelMap{
 			"schema": dbName,
 			"table":  tableName,
@@ -68,5 +83,7 @@ func addRowAndCheckRowsCount(t *testing.T, ctx context.Context, db *sql.DB, dbNa
 	}
 	// Variable got.value contains actual rows count in table.
 	// Should be equal to count of calling this method.
-	assert.Equal(t, expected, got)
+	if !reflect.DeepEqual(expect, got) {
+		t.Errorf("expected %+v, but got %+v", expect, got)
+	}
 }
